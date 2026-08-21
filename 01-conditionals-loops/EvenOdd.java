f1af36c9731a75cdadd1852c6742d8e09a5206eb
maheshkatti <katti.mahesh.babuu@gmail.com>
2026-08-21 15:41:44 +0530

    Organize files into concept-based folders: conditionals-loops, math, and string-building-methods

---
+++ 01-conditionals-loops/EvenOdd.java
@@ -0,0 +1,12 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 10;
+
+        // Check if number is even or odd
+        if (num % 2 == 0) {
+            System.out.println(num + " is an Even number");
+        } else {
+            System.out.println(num + " is an Odd number");
+        }
+    }
+}
---
+++ 01-conditionals-loops/Greeting.java
@@ -0,0 +1,9 @@
+public class Greeting {
+    public static void main(String[] args) {
+        String name = "User";
+        
+        System.out.println("Hello, " + name + "!");
+        System.out.println("Welcome to Java Programming!");
+        System.out.println("This is your first Java program.");
+    }
+}
